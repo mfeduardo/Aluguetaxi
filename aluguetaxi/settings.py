5a6972181c70ade180a729f2052f5e27a9f0dd28
Eduardo <82419748+mfeduardo@users.noreply.github.com>
2021-11-09 14:34:05 -0300

    dh

--- aluguetaxi/settings.py
+++ aluguetaxi/settings.py
@@ -12,10 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 import django_on_heroku
 from decouple import config
-import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 #BASE_DIR = Path(__file__).resolve().parent.parent

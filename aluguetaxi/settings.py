bd12c48c6ce6fcd5c0f008e3432dcafcbe4e8e11
Eduardo <82419748+mfeduardo@users.noreply.github.com>
2021-11-09 12:32:00 -0300

    env

--- aluguetaxi/settings.py
+++ aluguetaxi/settings.py
@@ -92,7 +92,13 @@ WSGI_APPLICATION = 'aluguetaxi.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-DATABASES = config('DATABASES')
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
 
 db_from_env = dj_database_url.config(conn_max_age=600)
 

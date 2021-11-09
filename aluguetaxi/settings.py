b42ca65b4ade13295ddf8020db6c5a57547a56c7
Eduardo <82419748+mfeduardo@users.noreply.github.com>
2021-11-09 13:22:20 -0300

    colect_static

--- aluguetaxi/settings.py
+++ aluguetaxi/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-DISABLE_COLLECTSTATIC=1
+
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
